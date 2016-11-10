4c51e3356514356211b5f8a3bfdc94fe739c7add
Raphael <raphael.beyerle@tuwien.ac.at>
2016-11-10 22:28:19 +0100

    Der VideoView füllt nun den gesamten Bildschirm aus

--- app/src/main/java/at/ac/tuwien/ims/neutronenstern/IntroActivity.java
+++ app/src/main/java/at/ac/tuwien/ims/neutronenstern/IntroActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
