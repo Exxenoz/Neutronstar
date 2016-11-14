192b92c057d566fbe74aa7198b82b64a8c49b87a
Andreas Krischke <andreas.krischke@tuwien.ac.at>
2016-11-14 23:00:28 +0100

    Immersive-Mode Code in onCreate() verschoben und Listener-Klasse für Laufzeitänderungen erstellt und registriert

--- app/src/main/java/at/autrage/projects/zeta/activity/SuperActivity.java
+++ app/src/main/java/at/autrage/projects/zeta/activity/SuperActivity.java
@@ -1,20 +1,40 @@
 package at.autrage.projects.zeta.activity;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class SuperActivity extends AppCompatActivity {
 
+    public final int WishedSystemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        decorView.setSystemUiVisibility(WishedSystemUiVisibility);
+        decorView.setOnSystemUiVisibilityChangeListener(new SystemUiVisibilityChangeListener(decorView));
+    }
+
+    private class SystemUiVisibilityChangeListener implements View.OnSystemUiVisibilityChangeListener {
+
+        View m_DecorView;
+
+        public SystemUiVisibilityChangeListener(View decorView) {
+            m_DecorView = decorView;
+        }
+
+        @Override
+        public void onSystemUiVisibilityChange(int visibility) {
+            if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                m_DecorView.setSystemUiVisibility(WishedSystemUiVisibility);
+            }
+        }
     }
 }
