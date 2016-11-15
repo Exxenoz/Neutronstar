9be8824aed3e0df96c32641c8d95fba51000365b
Andreas Krischke <andreas.krischke@tuwien.ac.at>
2016-11-16 00:06:10 +0100

    Ordentliches Skalierungssystem programmiert (erforderte Erhöhung der minSdkVersion von 15 auf 17)

--- app/src/main/java/at/autrage/projects/zeta/activity/SuperActivity.java
+++ app/src/main/java/at/autrage/projects/zeta/activity/SuperActivity.java
@@ -1,11 +1,22 @@
 package at.autrage.projects.zeta.activity;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Debug;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SuperActivity extends AppCompatActivity {
+
+    public final int ReferenceResolutionX = 1920;
+    public final int ReferenceResolutionY = 1080;
 
     public final int WishedSystemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -38,6 +49,85 @@ public class SuperActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         // Default: Do nothing
+    }
+
+    protected void scaleChildViewsToCurrentResolution(ViewGroup rootViewGroup) {
+        Log.d("PNE::Debug", "Activity Layout Name: " + getResources().getResourceEntryName(rootViewGroup.getId()));
+        Log.d("PNE::Debug", "Activity Layout Childs: " + rootViewGroup.getChildCount());
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point(); display.getRealSize(size);
+
+        int currResolutionX = size.x;
+        int currResolutionY = size.y;
+
+        float scaleFactorX = currResolutionX / (float)ReferenceResolutionX;
+        float scaleFactorY = currResolutionY / (float)ReferenceResolutionY;
+
+        float scaleFactor = Math.min(scaleFactorX, scaleFactorY);
+
+        Log.d("PNE::Debug", "Reference Resolution: " + ReferenceResolutionX + "x" + ReferenceResolutionY);
+        Log.d("PNE::Debug", "Current Resolution: " + currResolutionX + "x" + currResolutionY);
+        Log.d("PNE::Debug", "ScaleFactor (to current Resolution): " + scaleFactor);
+
+        int counter = 0;
+
+        if (scaleFactor == 1.0f) {
+            Log.d("PNE::Debug", "No scaling required, because scale factor is 1!");
+            return;
+        }
+
+        for (int i = 0, count = rootViewGroup.getChildCount(); i < count; ++i) {
+            View currView = rootViewGroup.getChildAt(i);
+            if (!(currView.getLayoutParams() instanceof ViewGroup.MarginLayoutParams)) {
+                continue;
+            }
+
+            if (!(currView instanceof ImageView ||
+                currView instanceof TextView ||
+                currView instanceof Button)) {
+                continue;
+            }
+
+            ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams)currView.getLayoutParams();
+
+            if (layoutParams.width > 0) {
+                layoutParams.width *= scaleFactor;
+            }
+
+            if (layoutParams.height > 0) {
+                layoutParams.height *= scaleFactor;
+            }
+
+            int leftMargin = layoutParams.leftMargin;
+            int rightMargin = layoutParams.rightMargin;
+            int topMargin = layoutParams.topMargin;
+            int bottomMargin = layoutParams.bottomMargin;
+
+            if (leftMargin > 0) {
+                leftMargin *= scaleFactor;
+            }
+
+            if (rightMargin > 0) {
+                rightMargin *= scaleFactor;
+            }
+
+            if (topMargin > 0) {
+                topMargin *= scaleFactor;
+            }
+
+            if (bottomMargin > 0) {
+                bottomMargin *= scaleFactor;
+            }
+
+            layoutParams.setMargins(leftMargin, topMargin, rightMargin, bottomMargin);
+
+            currView.setLayoutParams(layoutParams);
+
+            ++counter;
+        }
+
+        Log.d("PNE::Debug", "Scaled " + counter + " views to current resolution!");
     }
 
     protected void enableImmersiveMode(boolean setChangeListener) {
