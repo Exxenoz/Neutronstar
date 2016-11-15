295fd46dd3cedc304e74414e5780941117543100
Andreas Krischke <andreas.krischke@tuwien.ac.at>
2016-11-15 21:31:02 +0100

    Der ChangeListener für den Immersive-Mode wird jetzt nur mehr im onCreate() gesetzt (hoffentlich stapeln die set*Listener() Methoden die Objektreferenzen nicht, sondern fungieren so wie man das von einer set-Methode erwartet)

--- app/src/main/java/at/autrage/projects/zeta/activity/SuperActivity.java
+++ app/src/main/java/at/autrage/projects/zeta/activity/SuperActivity.java
@@ -1,6 +1,7 @@
 package at.autrage.projects.zeta.activity;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -14,17 +15,24 @@ public class SuperActivity extends AppCompatActivity {
             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
     @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        enableImmersiveMode(true);
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
 
-        enableImmersiveMode();
+        enableImmersiveMode(false);
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
 
-        enableImmersiveMode();
+        enableImmersiveMode(false);
     }
 
     @Override
@@ -32,10 +40,12 @@ public class SuperActivity extends AppCompatActivity {
         // Default: Do nothing
     }
 
-    protected void enableImmersiveMode() {
+    protected void enableImmersiveMode(boolean setChangeListener) {
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(WishedSystemUiVisibility);
-        decorView.setOnSystemUiVisibilityChangeListener(new SystemUiVisibilityChangeListener(decorView));
+        if (setChangeListener) {
+            decorView.setOnSystemUiVisibilityChangeListener(new SystemUiVisibilityChangeListener(decorView));
+        }
     }
 
     private class SystemUiVisibilityChangeListener implements View.OnSystemUiVisibilityChangeListener {
