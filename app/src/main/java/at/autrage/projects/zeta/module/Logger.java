ed0607521ac7f2e30aea17fc6753feac1b80edec
Raphael <raphael.beyerle@tuwien.ac.at>
2016-12-02 17:28:55 +0100

    Kürzung der Funktionsnamen des Logger Moduls

--- app/src/main/java/at/autrage/projects/zeta/module/Logger.java
+++ app/src/main/java/at/autrage/projects/zeta/module/Logger.java
@@ -6,15 +6,15 @@ public final class Logger {
 
     private Logger() {}
 
-    public static void Error(String message, Object... values) {
-        Log.e("PNE::ERR", String.format(message, values));
+    public static void E(String message, Object... values) {
+        Log.e("PNE::E", String.format(message, values));
     }
 
-    public static void Warning(String message, Object... values) {
-        Log.w("PNE::WRN", String.format(message, values));
+    public static void W(String message, Object... values) {
+        Log.w("PNE::W", String.format(message, values));
     }
 
-    public static void Debug(String message, Object... values) {
-        Log.d("PNE::DBG", String.format(message, values));
+    public static void D(String message, Object... values) {
+        Log.d("PNE::D", String.format(message, values));
     }
 }
