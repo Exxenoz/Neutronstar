99171f6884ba571d610876ced660704dc05454a0
Raphael <raphael.beyerle@tuwien.ac.at>
2016-12-20 00:20:36 +0100

    WeaponUpgrades- und Weapons-Enum hinzugefügt

---
+++ app/src/main/java/at/autrage/projects/zeta/model/WeaponUpgrades.java
@@ -0,0 +1,13 @@
+package at.autrage.projects.zeta.model;
+
+
+public enum WeaponUpgrades {
+    None,
+    IncreaseDamage,
+    IncreaseSpeed,
+    IncreaseRadius,
+    ResearchNuke,
+    ResearchLaser,
+    ResearchContactBomb,
+    ProBabyPill
+}
---
+++ app/src/main/java/at/autrage/projects/zeta/model/Weapons.java
@@ -0,0 +1,14 @@
+package at.autrage.projects.zeta.model;
+
+
+public enum Weapons {
+    None,
+    SmallRocket,
+    BigRocket,
+    SmallNuke,
+    BigNuke,
+    SmallLaser,
+    BigLaser,
+    SmallContactBomb,
+    BigContactBomb
+}
