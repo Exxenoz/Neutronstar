119e8d6e52265fe2eeb42edb2631dcd200b76d80
Raphael <raphael.beyerle@tuwien.ac.at>
2016-12-19 17:16:10 +0100

    Shopview Frontend erstellt

---
+++ app/src/main/java/at/autrage/projects/zeta/activity/ShopActivity.java
@@ -0,0 +1,13 @@
+package at.autrage.projects.zeta.activity;
+
+import android.os.Bundle;
+import at.autrage.projects.zeta.R;
+
+public class ShopActivity extends SuperActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+    }
+}
