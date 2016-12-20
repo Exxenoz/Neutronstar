379c523c14591a28785e74b2bc4b71b5c4d2df3f
Raphael <raphael.beyerle@tuwien.ac.at>
2016-12-20 22:13:40 +0100

    Basisdefinitionen von Waffen hinzugefügt

--- app/src/main/java/at/autrage/projects/zeta/module/Pustafin.java
+++ app/src/main/java/at/autrage/projects/zeta/module/Pustafin.java
@@ -9,4 +9,22 @@ public class Pustafin {
 
     public static final int SmallLaserCostPerSecond = 10;
     public static final int BigLaserCostPerSecond = 20;
+
+    public static final float SmallRocketHitDamageBase = 1f;
+    public static final float BigRocketHitDamageBase = 1f;
+    public static final float SmallNukeHitDamageBase = 1f;
+    public static final float BigNukeHitDamageBase = 1f;
+    public static final float SmallLaserHitDamageBase = 1f;
+    public static final float BigLaserHitDamageBase = 1f;
+    public static final float SmallContactBombHitDamageBase = 1f;
+    public static final float BigContactBombHitDamageBase = 1f;
+
+    public static final float SmallRocketSpeedBase = 1f;
+    public static final float BigRocketSpeedBase = 1f;
+    public static final float SmallNukeSpeedBase = 1f;
+    public static final float BigNukeSpeedBase = 1f;
+    public static final float SmallLaserSpeedBase = 0f;
+    public static final float BigLaserSpeedBase = 0f;
+    public static final float SmallContactBombSpeedBase = 1f;
+    public static final float BigContactBombSpeedBase = 1f;
 }
