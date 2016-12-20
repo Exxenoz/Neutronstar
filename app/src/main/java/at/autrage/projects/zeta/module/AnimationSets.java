68fd4615053018e53dabc84003a235c9fa4c1980
Raphael <raphael.beyerle@tuwien.ac.at>
2016-12-20 22:15:04 +0100

    Enums für Animationsets und Animations hinzugefügt

---
+++ app/src/main/java/at/autrage/projects/zeta/module/AnimationSets.java
@@ -0,0 +1,16 @@
+package at.autrage.projects.zeta.module;
+
+
+public enum AnimationSets {
+    BackgroundGame,
+    Planet,
+    Clouds,
+    SmallRocket,
+    BigRocket,
+    SmallNuke,
+    BigNuke,
+    SmallLaser,
+    BigLaser,
+    SmallContactBomb,
+    BigContactBomb
+}
---
+++ app/src/main/java/at/autrage/projects/zeta/module/Animations.java
@@ -0,0 +1,15 @@
+package at.autrage.projects.zeta.module;
+
+public enum Animations {
+    BackgroundGameDefault,
+    PlanetSheet,
+    CloudSheet,
+    SmallRocket,
+    BigRocket,
+    SmallNuke,
+    BigNuke,
+    SmallLaser,
+    BigLaser,
+    SmallContactBomb,
+    BigContactBomb
+}
