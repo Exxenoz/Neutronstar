f9e9d2b1df1982cb797c5b2a35435dafdc4beecb
Andreas Krischke <exxenoz@gmx.at>
2017-02-11 22:54:21 +0100

    PNE-5: Color Klasse (als Wrapper von einem float Array, das die Farbwerte im Intervall [0, 1] repräsentiert) hinzugefügt

---
+++ app/src/main/java/at/autrage/projects/zeta/opengl/Color.java
@@ -0,0 +1,30 @@
+package at.autrage.projects.zeta.opengl;
+
+public class Color {
+    private float[] m_Color;
+
+    public Color() {
+        m_Color = new float[] { 1f, 1f, 1f, 1f };
+    }
+
+    public Color(float r, float g, float b, float a) {
+        m_Color = new float[] { r, g, b, a };
+    }
+
+    public void setColor(float r, float g, float b, float a) {
+        m_Color[0] = r;
+        m_Color[1] = g;
+        m_Color[2] = b;
+        m_Color[3] = a;
+    }
+
+    public float[] getColor() {
+        return m_Color;
+    }
+
+    public static final float[] Red   = new float[] { 1f, 0f, 0f, 1f };
+    public static final float[] Green = new float[] { 0f, 1f, 0f, 1f };
+    public static final float[] Blue  = new float[] { 0f, 0f, 1f, 1f };
+    public static final float[] Black = new float[] { 0f, 0f, 0f, 1f };
+    public static final float[] White = new float[] { 1f, 1f, 1f, 1f };
+}
