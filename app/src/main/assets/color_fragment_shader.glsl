98ce1f74f8bd254a865475cc006adb6afa75826d
Markus Rezai <markus.rezai@gmail.com>
2017-02-24 23:08:12 +0100

    PNE-15: Float Precision zu Fragmentshadern hinzugefügt, da diese per Default keine haben.

--- app/src/main/assets/color_fragment_shader.glsl
+++ app/src/main/assets/color_fragment_shader.glsl
@@ -1,3 +1,5 @@
+precision mediump float;
+
 varying vec4 v_Color;          	// This is the color from the vertex shader interpolated across the
   								// triangle per fragment.
 
--- app/src/main/assets/sprite_fragment_shader.glsl
+++ app/src/main/assets/sprite_fragment_shader.glsl
@@ -1,3 +1,5 @@
+precision mediump float;
+
 uniform sampler2D u_Texture;    // The input texture.
 
 varying vec4 v_Color;           // This is the color from the vertex shader interpolated across the
