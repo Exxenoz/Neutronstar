cf73062bd5dad151c62a568e14234ce5ccf902e1
Andreas Krischke <exxenoz@gmx.at>
2017-04-08 21:31:56 +0200

    PNE-40: CalculateNormalisedTextureCoordinates() in die CoordinateTranslator-Klasse verschoben

--- app/src/main/java/at/autrage/projects/zeta/util/CoordinateTranslator.java
+++ app/src/main/java/at/autrage/projects/zeta/util/CoordinateTranslator.java
@@ -1,6 +1,7 @@
 package at.autrage.projects.zeta.util;
 
 import at.autrage.projects.zeta.activity.SuperActivity;
+import at.autrage.projects.zeta.module.Logger;
 import at.autrage.projects.zeta.module.Pustafin;
 
 public final class CoordinateTranslator {
@@ -10,5 +11,31 @@ public final class CoordinateTranslator {
 
     public static float ScreenCoordYToWorldCoordY(float screenCoordY) {
         return screenCoordY * SuperActivity.getScaleFactorY() - Pustafin.HalfReferenceResolutionY;
+    }
+
+    public static float[] CalculateNormalisedTextureCoordinates(int x, int y, int w, int h, int textureWidth, int textureHeight) {
+        if (textureWidth == 0 || textureHeight == 0) {
+            Logger.E("Could not calculate normalised texture coordinates, because texture width and height may not be zero!");
+            return new float[8];
+        }
+
+        float texCoordTopLeftX = x;
+        float texCoordTopLeftY = y;
+
+        float texCoordTopRightX = texCoordTopLeftX + w;
+        float texCoordTopRightY = texCoordTopLeftY;
+
+        float texCoordBottomLeftX = x;
+        float texCoordBottomLeftY = texCoordTopLeftY + h;
+
+        float texCoordBottomRightX = texCoordBottomLeftX + w;
+        float texCoordBottomRightY = texCoordBottomLeftY;
+
+        return new float[] {
+                texCoordBottomLeftX / textureWidth, texCoordBottomLeftY / textureHeight,     // Bottom Left
+                texCoordBottomRightX / textureWidth, texCoordBottomRightY / textureHeight,   // Bottom Right
+                texCoordTopLeftX / textureWidth, texCoordTopLeftY / textureHeight,           // Top Left
+                texCoordTopRightX / textureWidth, texCoordTopRightY / textureHeight          // Top Right
+        };
     }
 }
